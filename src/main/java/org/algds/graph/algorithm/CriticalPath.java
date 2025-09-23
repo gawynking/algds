80909cb99cad3c79c612228ad2ef1edea8341f4a
chavinking <chavinking1990@gmail.com>
2025-09-23 21:21:23 +0800

    更新算法实现

---
+++ src/main/java/org/algds/graph/algorithm/CriticalPath.java
@@ -0,0 +1,239 @@
+package org.algds.graph.algorithm;
+
+import org.algds.graph.adjacency.AdjacencyGraph;
+import org.algds.graph.adjacency.Edge;
+import org.algds.graph.adjacency.Label;
+import org.algds.graph.adjacency.Vertex;
+
+import java.util.*;
+
+/**
+ * 求AOE-网关键路径算法实现
+ * 输入数据结构: adjacency.AdjacencyGraph
+ */
+public class CriticalPath {
+
+    /**
+     * 求AOE-网关键路径
+     * 算法介绍：
+     * 1 对输入图执行拓扑排序，在拓扑排序过程中计算每个事件节点最早开始时间ve
+     * 2 针对执行完成拓扑排序的有序图执行逆拓扑排序顺序遍历，在遍历过程中计算每个事件节点最晚开始事件vl
+     * 3 执行完成拓扑排序遍历和逆拓扑排序遍历后，针对ve=vl的节点即为关键节点
+     *
+     * @param graph
+     */
+    public static List<Vertex> criticalPath(AdjacencyGraph graph) {
+
+        TopSortResult topSort = topSortKahn(graph);
+        if (null == topSort) {
+            return null;
+        }
+
+        Stack<Vertex> topSortStack = topSort.getTopSort();
+        Map<Integer, Integer> ve = topSort.getVe(); // 存储事件节点最早开始时间ve
+        Map<Integer, Integer> vl = new HashMap<>(); // 存储事件节点最晚开始事件vl
+
+        Integer maxCost = topSort.getMaxCost();
+
+        List<Vertex> creticalPathList = new ArrayList<>();
+        // 执行逆拓扑排序遍历
+        while (!topSortStack.isEmpty()) {
+            Vertex vertex = topSortStack.pop();
+            vl.put(vertex.getId(), vl.getOrDefault(vertex.getId(), ve.get(vertex.getId()))); // 初始化用最早完成时间
+            Integer lCost = maxCost;
+            for (Edge adj : vertex.getEdgeList()) {
+                Vertex to = adj.getTo();
+                Integer newLCost = vl.getOrDefault(to.getId(), ve.get(to.getId())) - adj.getWeight(); // 后继节点最晚开始时间 - 弧成本
+                lCost = Math.min(lCost, newLCost);
+                vl.put(vertex.getId(), lCost);
+            }
+        }
+
+        // 计算关键路径,ve=vl的事件节点即为关键路径节点
+        for (int j = 1; j <= graph.getVertexNum(); j++) {
+            if (ve.get(j) == vl.get(j)) {
+                creticalPathList.add(graph.getVertices().get(j));
+            }
+        }
+
+        return creticalPathList;
+    }
+
+
+    public static TopSortResult topSortKahn(AdjacencyGraph graph) {
+
+        // 1. 计算每个节点的入度 时间复杂度 V+E
+        Map<Integer, Integer> indegree = new HashMap<>();
+        for (Integer id : graph.getVertices().keySet()) {
+            indegree.putIfAbsent(id, 0);
+            for (Edge edge : graph.getVertices().get(id).getEdgeList()) {
+                indegree.put(edge.getTo().getId(), indegree.getOrDefault(edge.getTo().getId(), 0) + 1);
+            }
+        }
+
+        // 2. 所有入度为 0 的节点入队 时间复杂度 V
+        Queue<Integer> queue = new LinkedList<>();
+        for (Map.Entry<Integer, Integer> entry : indegree.entrySet()) {
+            if (entry.getValue() == 0) {
+                queue.add(entry.getKey());
+            }
+        }
+
+
+        // 3. 进行拓扑排序 时间复杂度 V+E
+        Stack<Vertex> topSortStack = new Stack<>();
+        Map<Integer, Integer> ve = new HashMap<>(); // 关键路径扩展结构，存储每个节点最早开始时间
+        Integer maxCost = 0;
+        while (!queue.isEmpty()) {
+            Integer vertexId = queue.poll();
+            Vertex vertex = graph.getVertices().get(vertexId);
+            topSortStack.push(vertex);
+            ve.put(vertexId, ve.getOrDefault(vertexId, 0));
+
+            List<Edge> neighbors = vertex.getEdgeList();
+            for (Edge neighbor : neighbors) {
+                Vertex to = neighbor.getTo();
+                indegree.put(to.getId(), indegree.get(to.getId()) - 1);
+                if (indegree.get(to.getId()) == 0) {
+                    queue.add(to.getId());
+                }
+                // 关键路径扩展-计算节点最早开始时间
+                if (ve.getOrDefault(vertex.getId(), 0) + neighbor.getWeight() > ve.getOrDefault(to.getId(), 0)) {
+                    ve.put(to.getId(), ve.getOrDefault(vertex.getId(), 0) + neighbor.getWeight());
+                    maxCost = ve.get(to.getId());
+                }
+            }
+        }
+
+        // 4. 如果排序结果数量 < 节点总数，说明有环
+        if (topSortStack.size() != indegree.size()) {
+            return null;
+        }
+
+        // 5. 封装返回结构
+        return new TopSortResult(topSortStack, ve, maxCost);
+    }
+
+
+    /**
+     * 单元测试
+     *          v2        v7
+     *        /| \      /| \
+     *      6/    \1  9/   2\
+     *      /      \| /      \|
+     *     v1        v5        v9
+     *     | \      /| \      /|
+     *    5| 4\   1/   7\   4/
+     *     |   \| /      \| /
+     *     |    v3        v8
+     *     |             /|
+     *      \|    2    4/
+     *        v4 ----> v6
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        // 构建图
+        AdjacencyGraph graph = new AdjacencyGraph();
+
+        // 创建标签
+        Label vertexLabel = new Label("V", 1);
+        Label edgeLabel = new Label("E", 2);
+
+        // 构造顶点
+        Vertex v1 = new Vertex("v1", vertexLabel);
+        Vertex v2 = new Vertex("v2", vertexLabel);
+        Vertex v3 = new Vertex("v3", vertexLabel);
+        Vertex v4 = new Vertex("v4", vertexLabel);
+        Vertex v5 = new Vertex("v5", vertexLabel);
+        Vertex v6 = new Vertex("v6", vertexLabel);
+        Vertex v7 = new Vertex("v7", vertexLabel);
+        Vertex v8 = new Vertex("v8", vertexLabel);
+        Vertex v9 = new Vertex("v9", vertexLabel);
+
+        graph.addVertex(v1);
+        graph.addVertex(v2);
+        graph.addVertex(v3);
+        graph.addVertex(v4);
+        graph.addVertex(v5);
+        graph.addVertex(v6);
+        graph.addVertex(v7);
+        graph.addVertex(v8);
+        graph.addVertex(v9);
+
+        // 构造边
+        graph.addEdge(v1, v2, 6, edgeLabel);
+        graph.addEdge(v1, v3, 4, edgeLabel);
+        graph.addEdge(v1, v4, 5, edgeLabel);
+
+        graph.addEdge(v2, v5, 1, edgeLabel);
+
+        graph.addEdge(v3, v5, 1, edgeLabel);
+
+        graph.addEdge(v4, v6, 2, edgeLabel);
+
+        graph.addEdge(v5, v7, 9, edgeLabel);
+        graph.addEdge(v5, v8, 7, edgeLabel);
+
+        graph.addEdge(v6, v8, 4, edgeLabel);
+
+        graph.addEdge(v7, v9, 2, edgeLabel);
+        graph.addEdge(v8, v9, 4, edgeLabel);
+
+        // 构造环
+//        graph.addEdge(v7,v5,1,edgeLabel);
+
+        // 求关键路径
+        List<Vertex> criticalPath = criticalPath(graph);
+        if (null == criticalPath) {
+            System.out.println("输入图存在环,无法计算关键路径.");
+        } else {
+            System.out.print("关键路径: ");
+            for (Vertex vertex : criticalPath) {
+                System.out.print(vertex.getName() + " -> ");
+            }
+            System.out.println("end.");
+        }
+
+    }
+
+
+    /**
+     * 封装拓扑排序返回结果,为关键路径求解输入
+     */
+    public static class TopSortResult {
+        Stack<Vertex> topSort; // 拓扑排序栈
+        Map<Integer, Integer> ve; // 节点最早开始时间
+        Integer maxCost; // 最晚完成时间 = 最后节点最早开始时间
+
+        public TopSortResult(Stack<Vertex> result, Map<Integer, Integer> ve, Integer maxCost) {
+            this.topSort = result;
+            this.ve = ve;
+            this.maxCost = maxCost;
+        }
+
+        public Stack<Vertex> getTopSort() {
+            return topSort;
+        }
+
+        public void setTopSort(Stack<Vertex> topSort) {
+            this.topSort = topSort;
+        }
+
+        public Map<Integer, Integer> getVe() {
+            return ve;
+        }
+
+        public void setVe(Map<Integer, Integer> ve) {
+            this.ve = ve;
+        }
+
+        public Integer getMaxCost() {
+            return maxCost;
+        }
+
+        public void setMaxCost(Integer maxCost) {
+            this.maxCost = maxCost;
+        }
+    }
+}
---
+++ src/main/java/org/algds/graph/algorithm/FloydAlgorithm.java
@@ -0,0 +1,205 @@
+package org.algds.graph.algorithm;
+
+import org.algds.graph.adjacency.AdjacencyGraph;
+import org.algds.graph.adjacency.Edge;
+import org.algds.graph.adjacency.Label;
+import org.algds.graph.adjacency.Vertex;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 弗洛伊德算法,求多源最短路径
+ *  使用邻接表数据结构构造图，在执行floyd算法时首先将邻接表转换成索引实现邻接矩阵
+ */
+public class FloydAlgorithm {
+
+    // 使用INF表示两个顶点之间没有直接连接
+    final static int INF = Integer.MAX_VALUE;
+
+    /**
+     * 使用Floyd算法计算所有顶点对之间的最短路径
+     */
+    public static Map<String, int[][]> floyd(AdjacencyGraph graph) {
+
+        // 通过邻接表构造邻接矩阵
+//        int[][] metrix = {
+//                {0, 3, INF, 7},
+//                {8, 0, 2, INF},
+//                {5, INF, 0, 1},
+//                {2, INF, INF, 0}
+//        };
+        int[][] metrix = new int[graph.getVertexNum()][graph.getVertexNum()];
+        for (int i = 0; i < metrix.length; i++) { // 初始化数组
+            for (int j = 0; j < metrix.length; j++) {
+                if (i == j) {
+                    metrix[i][j] = 0;
+                } else {
+                    metrix[i][j] = INF;
+                }
+            }
+        }
+        Map<Integer, Vertex> vertices = graph.getVertices();
+        for (Map.Entry<Integer, Vertex> entry : vertices.entrySet()) {
+            Integer i = entry.getKey();
+            List<Edge> edgeList = entry.getValue().getEdgeList();
+            for (Edge edge : edgeList) {
+                Integer j = edge.getTo().getId();
+                metrix[i - 1][j - 1] = edge.getWeight();
+            }
+        }
+
+
+        // 距离矩阵变化趋势追踪
+        System.out.println("距离矩阵变化趋势追踪: ");
+
+        // 打印原始矩阵
+        printSolution(metrix, -1);
+
+        int n = metrix.length;
+        int[][] dist = new int[n][n];
+        int[][] next = new int[n][n]; // next[i][j]表示在顶点i到顶点j的最短路径上，顶点i的下一个顶点索引
+
+        // 初始化矩阵
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dist[i][j] = metrix[i][j];
+                if (dist[i][j] != Integer.MAX_VALUE) {
+                    next[i][j] = j; // 初始化,顶点i到顶点j可达,初始可以顶点i的下一个顶点信息为j
+                } else {
+                    next[i][j] = -1; // 初始化,没有下一个顶点信息,表示i->j不可达
+                }
+            }
+        }
+
+        // 应用Floyd算法
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    /**
+                     * 如果通过顶点k的路径更短，则更新距离
+                     */
+                    if (dist[i][k] != Integer.MAX_VALUE && dist[k][j] != Integer.MAX_VALUE && dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                        next[i][j] = next[i][k];
+                        // 每次更新矩阵都将大新最新距离矩阵,下一次更新要在上一次距离矩阵基础上迭代
+                        printSolution(dist, k);
+                    }
+                }
+            }
+        }
+
+
+        // 返回处理结果
+        Map<String, int[][]> result = new HashMap<>();
+        result.put("dist", dist);
+        result.put("next", next);
+        return result;
+    }
+
+
+    /**
+     * 示例图
+     * 3
+     * 0 <---> 1
+     * /|| \ 8  |
+     * 2||7 \ 5 |2
+     * ||/  \|  |/
+     * 3 <---> 3
+     * 1
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        // 构建图
+        AdjacencyGraph graph = new AdjacencyGraph();
+
+        // 创建标签
+        Label vertexLabel = new Label("V", 1);
+        Label edgeLabel = new Label("E", 2);
+
+        // 构造顶点
+        Vertex v1 = new Vertex("v1", vertexLabel);
+        Vertex v2 = new Vertex("v2", vertexLabel);
+        Vertex v3 = new Vertex("v3", vertexLabel);
+        Vertex v4 = new Vertex("v4", vertexLabel);
+
+        graph.addVertex(v1);
+        graph.addVertex(v2);
+        graph.addVertex(v3);
+        graph.addVertex(v4);
+
+
+        // 构造边
+        graph.addEdge(v1, v2, 3, edgeLabel);
+        graph.addEdge(v1, v4, 7, edgeLabel);
+
+        graph.addEdge(v2, v3, 2, edgeLabel);
+        graph.addEdge(v2, v1, 8, edgeLabel);
+
+        graph.addEdge(v3, v1, 5, edgeLabel);
+        graph.addEdge(v3, v4, 1, edgeLabel);
+
+        graph.addEdge(v4, v1, 2, edgeLabel);
+
+
+
+        // 应用Floyd算法
+        Map<String, int[][]> floyd = floyd(graph);
+
+
+        // 打印多源最短路径
+        System.out.println("============================================================================");
+        int[][] nexts = floyd.get("next");
+        printShortestPath(graph, nexts);
+
+    }
+
+    // 打印多源最短路径
+    private static void printShortestPath(AdjacencyGraph graph, int[][] next) {
+        int n = next.length;
+        for (int i = 0; i < n; i++) {
+            System.out.println(String.format("源点: %s", graph.getVertexName(i + 1)));
+            for (int j = 0; j < n; j++) {
+                System.out.print(String.format("    %s -> %s 最短路径 : %s -> ", graph.getVertexName(i + 1), graph.getVertexName(j + 1), graph.getVertexName(i + 1)));
+                int index = next[i][j];
+                while (index != -1 && index != i && index != j) {
+                    System.out.print(String.format("%s -> ", graph.getVertexName(index + 1)));
+                    index = next[index][j];
+                }
+                if (index != i) {
+                    System.out.println(String.format("%s -> ", graph.getVertexName(index + 1)));
+                } else {
+                    System.out.println();
+                }
+            }
+        }
+    }
+
+    // 打印最短距离矩阵
+    private static void printSolution(int[][] dist, int k) {
+        int n = dist.length;
+
+        if (k == -1) {
+            System.out.println("原始图的邻接矩阵:");
+        } else if (k == -2) {
+            System.out.println("所有顶点对之间的最短距离矩阵:");
+        } else {
+            System.out.println("所有顶点对之间的最短距离矩阵: k = " + k);
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dist[i][j] == Integer.MAX_VALUE) {
+                    System.out.print("INF\t");
+                } else {
+                    System.out.print(dist[i][j] + "\t");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
